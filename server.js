0eecbc4c67ddb488438ad83e82a3462a28f385df
jamesemery001 <james.emery001@umb.edu>
2026-03-23 17:13:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.post("/register", async function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
