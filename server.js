0f3a4de51102acee76dd0c87da9b32c842a25483
jamesemery001 <james.emery001@umb.edu>
2026-04-01 16:12:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -63,8 +63,8 @@ app.post("/register", async function (req, res) {
 });
 
 app.post("/login", async function(req, res) {
-  const userName = req.query.username;
-  const passWord = req.query.password;
+  const userName = req.body.username;
+  const passWord = req.body.password;
 
   try {
     const findUsername = "SELECT * FROM users WHERE username = $1";
