e78420683a6d81588b0e9381c1c1def4197b0b2c
jamesemery001 <james.emery001@umb.edu>
2026-04-01 16:56:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ async function createUsersTable() {
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL
       )
     `);
